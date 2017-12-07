cb34332bf8678bfe7f0412e0169138da23f97404
PhilippeKuret <philippekuret@gmail.com>
2017-12-07 20:42:50 +0100

    Plan details activity created

---
+++ app/src/main/java/com/example/philippe/gymtools/Activities/PlanDetails.java
@@ -0,0 +1,17 @@
+package com.example.philippe.gymtools.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.philippe.gymtools.R;
+
+public class PlanDetails extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_plan_details);
+	}
+}
