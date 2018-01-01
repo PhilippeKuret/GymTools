b2c2a3338beca54fd74d9aace58fa3cf34c3a13d
PhilippeKuret <philippekuret@gmail.com>
2018-01-01 17:40:12 -0500

    Created workoutservice

---
+++ app/src/main/java/com/example/philippe/gymtools/Module/WorkoutService.java
@@ -0,0 +1,46 @@
+package com.example.philippe.gymtools.Module;
+
+import android.content.Context;
+
+import com.example.philippe.gymtools.Objects.Workout;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class WorkoutService
+{
+	private final AppDatabase appDatabase;
+
+	public WorkoutService(Context context)
+	{
+		appDatabase = AppDatabase.getAppDatabase(context);
+	}
+
+	public Single<List<Workout>> getWorkouts(int exerciseId)
+	{
+		return appDatabase.workoutDao().getWorkout(exerciseId)
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread());
+	}
+
+	public Single<Object> createWorkout(Workout workout)
+	{
+		return Single.fromObservable(observer -> {
+			appDatabase.workoutDao().insertWorkout(workout);
+		})
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread());
+	}
+
+	public Single<Object> deleteWorkout(Workout workout)
+	{
+		return Single.fromObservable(observer -> {
+			appDatabase.workoutDao().deleteWorkout(workout);
+		})
+				.subscribeOn(Schedulers.io())
+				.observeOn(AndroidSchedulers.mainThread());
+	}
+}
---
+++ app/src/main/java/com/example/philippe/gymtools/Module/WorkoutServiceInterface.java
@@ -0,0 +1,16 @@
+package com.example.philippe.gymtools.Module;
+
+import com.example.philippe.gymtools.Objects.Workout;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+public interface WorkoutServiceInterface
+{
+	Single<List<Workout>> getWorkouts(int exerciseId);
+
+	Single<Object> createWorkout(Workout workout);
+
+	Single<Object> deleteWorkout(Workout workout);
+}
